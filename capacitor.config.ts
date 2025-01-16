2c0f71195f0729151cbe5d6d30651e9443fab1dd
zaijan <jgodito27@gmail.com>
2025-01-16 12:55:40 +0800

    add ios and android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,18 @@ const config: CapacitorConfig = {
   android: {
     flavor: "qa",
   },
+  plugins: {
+    LiveUpdates: {
+      appId: '225a6279',
+      channel: 'Development',
+      autoUpdateMethod: 'none',
+      maxVersions: 2
+    },
+    SplashScreen: {
+      launchAutoHide: false
+    }
+  }
 };
 
 export default config;
+ 
